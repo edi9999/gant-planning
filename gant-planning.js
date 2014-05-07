21e219028eeed9e82a2356c1a98f27f98f02864e
edi9999 <hipp.edg@gmail.com>
2014-05-07 12:19:26 +0200

    first visual of the planning

--- gant-planning.js
+++ gant-planning.js
@@ -3,5 +3,50 @@ var gantplanning={
 	version:"0.0.1"
 };
 
+var Planning=function()
+{
+
+	this.style={
+		phasesColor:d3.rgb("#083968"),
+		phasesHeight:"15px"
+	};
+	
+	this.fill=function(events)
+	{
+		this.events=events;
+		return this;
+	}
+
+	this.attachTo=function(element)
+	{
+		this.element=element;
+		this.mainElement= d3.select(this.element)
+			.attr("class","gant-planning")
+		return this;
+	}
+
+	this.draw=function()
+	{
+		var phases=this.mainElement.selectAll("rect.phases")
+			.data(this.events);
+
+		phases.enter()
+			.append("rect")
+			.attr("class","phases")
+			.attr("fill",this.style.phasesColor)
+			.attr("height",this.style.phasesHeight)
+			.attr("width","15px")
+
+		return this;
+	}
+
+	this.events=[];
+}
+
+gantplanning.createPlanning=function(planning)
+{
+	return new Planning();
+}
+
 this.gantplanning=gantplanning;
 }()
