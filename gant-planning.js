28e3cf6e6c1d98272e05545f96b12c481e274c9c
edi9999 <hipp.edg@gmail.com>
2014-05-07 15:24:54 +0200

    drag and drop changes start and end

--- gant-planning.js
+++ gant-planning.js
@@ -19,6 +19,10 @@ var Planning=function()
 	this.fill=function(phases)
 	{
 		this.phases=phases;
+		for(i=0;i<this.phases.length;i++){
+			this.phases[i].dx=0;
+			this.phases[i].dy=0;
+		}
 		return this;
 	}
 
@@ -32,17 +36,19 @@ var Planning=function()
 
 	var drag = d3.behavior.drag()
 		.on("drag", function(d,i) {
-			console.log(d)
-			console.log(d.x)
 			d.dx+=d3.event.dx;
 			_this.mainElement.selectAll(".phase-"+i)
 				.attr("transform","translate("+[d.dx,0]+")");
 		})
 		.on("dragend",function(d,i)
 		{
-			for(i=0;i<_this.events.length;i++) {
-				_this.events[i].x+=_this.events[i].dx;
-				_this.events[i].dx=0;
+			for(i=0;i<_this.phases.length;i++) {
+				var phase=_this.phases[i];
+				phase.x+=phase.dx;
+				var newStart=Math.round(timeToCoordinate.invert(phase.x));
+				phase.end=newStart+phase.end-phase.start
+				phase.start=newStart
+				phase.dx=0;
 			}
 			reorderPhases();
 			_this.draw();
@@ -51,26 +57,29 @@ var Planning=function()
 	var reorderPhases=function()
 	{
 		_this.phases.sort(function(a,b){
-        	var x = a.start+1/100*a.end; var y = b.start+1/100*b.end;
-        	if (x<y) return -1;
-        	if (x>y) return 1;
+			var x = a.start+1/100*a.end; 
+			var y = b.start+1/100*b.end;
+			if (x<y) return -1;
+			if (x>y) return 1;
 			if (a.description<b.description) return -1;
 			if (a.description>b.description) return 1;
 			return -1;
-		})
+		});
 	}
+
+	var timeToCoordinate = d3.scale.linear()
+		.domain([1,2])
+		.range([0,_this.style.stepWidth]);
 
 	var calcCoordinates=function()
 	{
 		for(i=0;i<_this.phases.length;i++) {
-			var ev=_this.phases[i];
-			ev.x=(ev.start-1)*_this.style.stepWidth;
-			ev.y=_this.style.phasesY+i*_this.style.phasesHeight;
-			ev.dx=0;
-			ev.dy=0;
-			ev.textx=(ev.start+ev.end-2)/2*_this.style.stepWidth;
-			ev.texty=_this.style.phasesY+17+i*_this.style.phasesHeight;
-			_this.phases[i]=ev;
+			var phase=_this.phases[i];
+			phase.x=timeToCoordinate(phase.start);
+			phase.textx=timeToCoordinate((phase.start+phase.end)/2);
+			phase.y=_this.style.phasesY+i*_this.style.phasesHeight;
+			phase.texty=_this.style.phasesY+17+i*_this.style.phasesHeight;
+			_this.phases[i]=phase;
 		}
 	}
 
@@ -84,6 +93,8 @@ var Planning=function()
 			.attr("class",function(v,i){return "phase phase-"+i})
 			.attr("fill",_this.style.phasesColor)
 			.attr("height",_this.style.phasesHeight+"px")
+
+		phases
 			.attr("width",function(v,i){return (v.end-v.start)*_this.style.stepWidth+"px"})
 			.attr("transform","translate("+[0,0]+")")
 			.attr("x",function(v,i){return v.x+"px"})
@@ -99,19 +110,20 @@ var Planning=function()
 		descriptions.enter()
 			.append("text")
 			.attr("class",function(v,i){return "description phase-"+i})
-			.attr("x",function(v,i){return v.textx+"px"})
-			.attr("y",function(v,i){return v.texty+"px"})
-			.attr("fill",_this.style.textColor)
-			.attr("transform","translate("+[0,0]+")")
 			.attr("text-anchor","middle")
 			.attr("font-size",_this.style.fontsize)
+			.attr("fill",_this.style.textColor)
+
+		descriptions
+			.attr("transform","translate("+[0,0]+")")
+			.attr("x",function(v,i){return v.textx+"px"})
+			.attr("y",function(v,i){return v.texty+"px"})
 			.text(function(d){return d.description})
 			.call(drag);
 	}
 
 	this.draw=function()
 	{
-		var _this=this;
 		calcCoordinates();
 		updatePhases();
 		updateDescriptions();
