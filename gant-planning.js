b5c4c4e04a32b7fd2e2ef89db202802883e0f18f
edi9999 <hipp.edg@gmail.com>
2014-05-07 12:56:43 +0200

    moved out method draw phases and draw descriptions

--- gant-planning.js
+++ gant-planning.js
@@ -5,10 +5,15 @@ var gantplanning={
 
 var Planning=function()
 {
+	_this=this;
 
 	this.style={
 		phasesColor:d3.rgb("#083968"),
-		phasesHeight:"15px"
+		stepWidth:20,
+		phasesHeight:25,
+		textColor:d3.rgb("#F7F7F7"),
+		phasesY:30,
+		fontsize:16
 	};
 	
 	this.fill=function(events)
@@ -21,22 +26,46 @@ var Planning=function()
 	{
 		this.element=element;
 		this.mainElement= d3.select(this.element)
-			.attr("class","gant-planning")
+			.attr("class","gant-planning");
 		return this;
+	}
+
+	var updatePhases=function()
+	{
+		var phases=_this.mainElement.selectAll("rect.phase")
+			.data(_this.events);
+
+		phases.enter()
+			.append("rect")
+			.attr("class","phase")
+			.attr("fill",_this.style.phasesColor)
+			.attr("height",_this.style.phasesHeight+"px")
+			.attr("width",function(v,i){return (v.end-v.start)*_this.style.stepWidth+"px"})
+			.attr("x",function(v,i){return (v.start-1)*_this.style.stepWidth+"px"})
+			.attr("y",function(v,i){return _this.style.phasesY+i*_this.style.phasesHeight+"px"});
+	}
+
+	var updateDescriptions=function()
+	{
+		var descriptions=_this.mainElement.selectAll("text.description")
+			.data(_this.events);
+
+		descriptions.enter()
+			.append("text")
+			.attr("class","description")
+			.attr("x",function(v,i){return (v.start+v.end-2)/2*_this.style.stepWidth+"px"})
+			.attr("y",function(v,i){return _this.style.phasesY+17+(i)*_this.style.phasesHeight+"px"})
+			.attr("fill",_this.style.textColor)
+			.attr("text-anchor","middle")
+			.attr("font-size",_this.style.fontsize)
+			.text(function(d){return d.description});
 	}
 
 	this.draw=function()
 	{
-		var phases=this.mainElement.selectAll("rect.phases")
-			.data(this.events);
-
-		phases.enter()
-			.append("rect")
-			.attr("class","phases")
-			.attr("fill",this.style.phasesColor)
-			.attr("height",this.style.phasesHeight)
-			.attr("width","15px")
-
+		var _this=this;
+		updatePhases();
+		updateDescriptions();
 		return this;
 	}
 
