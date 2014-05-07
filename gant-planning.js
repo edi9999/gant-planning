7d2fe21782a6b3e0f8e46adfb21120482e081ce9
edi9999 <hipp.edg@gmail.com>
2014-05-07 13:45:07 +0200

    can move phases with drag and drop

--- gant-planning.js
+++ gant-planning.js
@@ -16,9 +16,9 @@ var Planning=function()
 		fontsize:16
 	};
 	
-	this.fill=function(events)
+	this.fill=function(phases)
 	{
-		this.events=events;
+		this.phases=phases;
 		return this;
 	}
 
@@ -32,35 +32,60 @@ var Planning=function()
 
 	var drag = d3.behavior.drag()
 		.on("drag", function(d,i) {
-			d.x+=d3.event.dx;
-			console.log(this);
-			_this.mainElement.selectAll(".phase-"+i).attr("transform","translate("+[d.x,0]+")");
-	});
+			console.log(d)
+			console.log(d.x)
+			d.dx+=d3.event.dx;
+			_this.mainElement.selectAll(".phase-"+i)
+				.attr("transform","translate("+[d.dx,0]+")");
+		})
+		.on("dragend",function(d,i)
+		{
+			for(i=0;i<_this.events.length;i++) {
+				_this.events[i].x+=_this.events[i].dx;
+				_this.events[i].dx=0;
+			}
+			reorderPhases();
+			_this.draw();
+		});
+
+	var reorderPhases=function()
+	{
+		_this.phases.sort(function(a,b){
+        	var x = a.start+1/100*a.end; var y = b.start+1/100*b.end;
+        	if (x<y) return -1;
+        	if (x>y) return 1;
+			if (a.description<b.description) return -1;
+			if (a.description>b.description) return 1;
+			return -1;
+		})
+	}
 
 	var calcCoordinates=function()
 	{
-		for(i=0;i<_this.events.length;i++) {
-			var ev=_this.events[i];
+		for(i=0;i<_this.phases.length;i++) {
+			var ev=_this.phases[i];
 			ev.x=(ev.start-1)*_this.style.stepWidth;
 			ev.y=_this.style.phasesY+i*_this.style.phasesHeight;
+			ev.dx=0;
+			ev.dy=0;
 			ev.textx=(ev.start+ev.end-2)/2*_this.style.stepWidth;
 			ev.texty=_this.style.phasesY+17+i*_this.style.phasesHeight;
-			_this.events[i]=ev;
+			_this.phases[i]=ev;
 		}
 	}
 
 	var updatePhases=function()
 	{
 		var phases=_this.mainElement.selectAll("rect.phase")
-			.data(_this.events);
+			.data(_this.phases);
 
 		phases.enter()
 			.append("rect")
-			.attr("class","phase")
-			.attr("class",function(v,i){return "phase-"+i})
+			.attr("class",function(v,i){return "phase phase-"+i})
 			.attr("fill",_this.style.phasesColor)
 			.attr("height",_this.style.phasesHeight+"px")
 			.attr("width",function(v,i){return (v.end-v.start)*_this.style.stepWidth+"px"})
+			.attr("transform","translate("+[0,0]+")")
 			.attr("x",function(v,i){return v.x+"px"})
 			.attr("y",function(v,i){return v.y+"px"})
 			.call(drag);
@@ -69,15 +94,15 @@ var Planning=function()
 	var updateDescriptions=function()
 	{
 		var descriptions=_this.mainElement.selectAll("text.description")
-			.data(_this.events);
+			.data(_this.phases);
 
 		descriptions.enter()
 			.append("text")
-			.attr("class","description")
-			.attr("class",function(v,i){return "phase-"+i})
+			.attr("class",function(v,i){return "description phase-"+i})
 			.attr("x",function(v,i){return v.textx+"px"})
 			.attr("y",function(v,i){return v.texty+"px"})
 			.attr("fill",_this.style.textColor)
+			.attr("transform","translate("+[0,0]+")")
 			.attr("text-anchor","middle")
 			.attr("font-size",_this.style.fontsize)
 			.text(function(d){return d.description})
@@ -93,7 +118,7 @@ var Planning=function()
 		return this;
 	}
 
-	this.events=[];
+	this.phases=[];
 }
 
 gantplanning.createPlanning=function(planning)
